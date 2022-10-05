4e7a15675b0f423485036edb8d698738a96b0732
Şule <sule.kaptan@bil.omu.edu.tr>
2022-10-06 00:58:05 +0300

    project done

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # RandomPasswordGenerator
-Live Server: 
+Live Server: https://sulekaptan-randompasswordgenerator.netlify.app/
 <br/>
 Project Link: https://scrimba.com/learn/learnjavascript/solo-project-password-generator-cR9B46Sg
 
 ### Images
+<img src="photo-1.png">
---
+++ index.js
@@ -0,0 +1,25 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let password1 = document.getElementById("password-1")
+let password2 = document.getElementById("password-2")
+let passwordLength = 8
+
+function getRandom(){
+    let randomIndex = Math.floor(Math.random() * characters.length)
+    return characters[randomIndex]
+}
+
+function generatePassword(){
+    let randomPassword = ""
+    for(let i = 0 ; i < passwordLength; i++){
+        randomPassword += getRandom()
+    }
+    return randomPassword
+}
+
+function onClick(){
+    password1.textContent = generatePassword()
+    password2.textContent = generatePassword()
+
+}
